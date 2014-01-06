e9924b051a737b57ecc1b380a3586316bd243661
kudin <kudinsasha@gmail.com>
2014-01-06 20:11:01 +0300

    исправли глюк в валидации KFTextRangeType

--- classes/usertypes/kftextrangetype.php
+++ classes/usertypes/kftextrangetype.php
@@ -2,15 +2,26 @@
  
 class KFTextRangeType extends kfiltertype {
  
-    function addVariants(&$field) {
-        if($_REQUEST[$field['NAME'] . '_FROM'] > $_REQUEST[$field['NAME'] . '_TO'])
-            $_REQUEST[$field['NAME'] . '_FROM'] = $_REQUEST[$field['NAME'] . '_TO'];
-        foreach (array('FROM' => '>=', 'TO' => '<=') as $key => $znak) { 
-            if($_REQUEST[$field['NAME'] . '_' . $key]){
-                $field['VALUE'][$key] = $_REQUEST[$field['NAME'] . '_' . $key]; 
-                $this->filter[ $znak . 'PROPERTY_' . $field['NAME'] ] = $_REQUEST[$field['NAME'] . '_' . $key]; 
-            }
+    function addVariants(&$field) { 
+        $_REQUEST[$field['NAME'] . '_FROM'] = intval($_REQUEST[$field['NAME'] . '_FROM']);
+        $_REQUEST[$field['NAME'] . '_TO'] = intval($_REQUEST[$field['NAME'] . '_TO']); 
+        if(!$_REQUEST[$field['NAME'] . '_FROM'] && !$_REQUEST[$field['NAME'] . '_TO']){
+            return;
         }
+        if( $_REQUEST[$field['NAME'] . '_FROM'] > $_REQUEST[$field['NAME'] . '_TO'] && $_REQUEST[$field['NAME'] . '_TO'] ){
+            $_REQUEST[$field['NAME'] . '_FROM'] = $_REQUEST[$field['NAME'] . '_TO'];
+        }
+        if($_REQUEST[$field['NAME'] . '_FROM'] == $_REQUEST[$field['NAME'] . '_TO']){ 
+            $field['VALUE']['FROM'] = $field['VALUE']['TO'] = $_REQUEST[$field['NAME'] . '_FROM']; 
+            $this->filter['PROPERTY_' . $field['NAME']] = $_REQUEST[$field['NAME'] . '_FROM']; 
+        } else {   
+            foreach (array('FROM' => '>=', 'TO' => '<=') as $key => $znak) { 
+                if($_REQUEST[$field['NAME'] . '_' . $key]){
+                    $field['VALUE'][$key] = $_REQUEST[$field['NAME'] . '_' . $key]; 
+                    $this->filter[$znak . 'PROPERTY_' . $field['NAME']] = $_REQUEST[$field['NAME'] . '_' . $key]; 
+                }
+            } 
+        }  
     }
- 
-} 
+    
+}
