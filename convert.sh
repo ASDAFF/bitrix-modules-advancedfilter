00be0fb943c2cd73a81d8c85b44577d199850ceb
Sasha <kudinsasha@gmail.com>
2014-01-20 15:43:32 +0200

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Конвертирует модули с utf-8 в cp1251
+# Конвертирует модуль с utf-8 в cp1251
  
 for i in `find ./ -type f -name '*.php'`; do
         iconv -f utf-8 -t cp1251 $i >> $i.utf
