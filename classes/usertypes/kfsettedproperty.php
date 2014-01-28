7276326068ecd5ad5e2c394586a8041c900a6a0e
kudin <kudinsasha@gmail.com>
2014-01-28 14:15:47 +0300

    исправил баг с неверным кодом для фильтра

--- classes/usertypes/kfsettedproperty.php
+++ classes/usertypes/kfsettedproperty.php
@@ -7,7 +7,7 @@ class KFSettedProperty extends kfiltertype {
     function addVariants(&$field) {
         if($_REQUEST[$field['NAME']] == 'Y') {
             $field['VALUE'] = true;
-            $this->filter['!PROPERTY_' . $field['PROPERTY']['CODE']] = false;
+            $this->filter['!PROPERTY_' . $field['PROPERTY']] = false;
         } 
     }
  
